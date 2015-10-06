055385194563207df1413a874e8df04ec3b4025c
Prototyping Test <prototestuser@telerik.local>
2015-10-06 06:51:58 +0000

    Changes from Screen Builder

---
+++ ODATAProviderGen10 Hybrid/components/dataListView/index.js
@@ -0,0 +1,53 @@
+'use strict';
+
+app.dataListView = kendo.observable({
+    onShow: function() {},
+    afterShow: function() {}
+});
+
+// START_CUSTOM_CODE_dataListView
+// END_CUSTOM_CODE_dataListView
+(function(parent) {
+    var dataProvider = app.data.odata,
+        dataSourceOptions = {
+            type: 'odata',
+            transport: {
+                read: {
+                    url: dataProvider.url
+                }
+            },
+
+            schema: {
+                model: {
+                    fields: {
+                        'ProductName': {
+                            field: 'ProductName',
+                            defaultValue: ''
+                        },
+                    }
+                }
+            },
+        },
+        dataSource = new kendo.data.DataSource(dataSourceOptions),
+        dataListViewModel = kendo.observable({
+            dataSource: dataSource,
+            itemClick: function(e) {
+                app.mobileApp.navigate('#components/dataListView/details.html?uid=' + e.dataItem.uid);
+            },
+            detailsShow: function(e) {
+                var item = e.view.params.uid,
+                    dataSource = dataListViewModel.get('dataSource'),
+                    itemModel = dataSource.getByUid(item);
+                if (!itemModel.ProductName) {
+                    itemModel.ProductName = String.fromCharCode(160);
+                }
+                dataListViewModel.set('currentItem', itemModel);
+            },
+            currentItem: null
+        });
+
+    parent.set('dataListViewModel', dataListViewModel);
+})(app.dataListView);
+
+// START_CUSTOM_CODE_dataListViewModel
+// END_CUSTOM_CODE_dataListViewModel
---
+++ ODATAProviderGen10 Hybrid/dataProviders/odata.js
@@ -0,0 +1,11 @@
+'use strict';
+
+(function() {
+    app.data.odata = {
+        url: 'http://services.odata.org/V2/Northwind/Northwind.svc/Products'
+    };
+
+}());
+
+// START_CUSTOM_CODE_odata
+// END_CUSTOM_CODE_odata
